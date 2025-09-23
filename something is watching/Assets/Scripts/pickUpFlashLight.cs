cd06c2d5a39d2ce5db6b6428e79a1bf54251783a
qqtry-gg <wojtek.bielak1@interia.pl>
2025-09-23 20:24:49 +0200

    added putting sticks to the firecamp

--- something is watching/Assets/Scripts/pickUpFlashLight.cs
+++ something is watching/Assets/Scripts/pickUpFlashLight.cs
@@ -1,22 +1,47 @@
+using TMPro;
 using UnityEngine;
+using UnityEngineInternal;
 
 public class pickUpFlashLight : MonoBehaviour
 {
     [SerializeField] GameObject flashLight;
+    [SerializeField] GameObject campFire;
+    [SerializeField] TextMeshProUGUI textMeshPro;
     [SerializeField] flashLightScript playerScript;
+    [SerializeField] gameManagerScript gameManagerScript;
+    [SerializeField] fireScript fireScript;
+    private void Start()
+    {
+        fireScript = GameObject.Find("Fire").GetComponent<fireScript>();
+    }
     void Update()
     {
         if (Input.GetKeyUp(KeyCode.G))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
             RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit, 5f)) 
+            if (Physics.Raycast(ray, out hit, 25f))
             {
                 if (hit.collider.gameObject == flashLight)
                 {
                     playerScript.canUseFlashLight = true;
                     Destroy(flashLight);
+                }
+                else if (hit.collider.CompareTag("FireWood"))
+                {
+                    Debug.Log("Hit: " + hit.collider.gameObject.name);
+                    gameManagerScript.sticks += 1;
+                    textMeshPro.text = "Sticks: "+gameManagerScript.sticks;
+                    Destroy(hit.collider.gameObject);
+                }
+                else if (hit.collider.gameObject == campFire)
+                {
+                    if (gameManagerScript.sticks > 0)
+                    {
+                        gameManagerScript.sticks -= 1;
+                        textMeshPro.text = "Sticks: " + gameManagerScript.sticks;
+                        fireScript.timeBeforeDisappearing += 10;
+                    }
                 }
             }
         }
