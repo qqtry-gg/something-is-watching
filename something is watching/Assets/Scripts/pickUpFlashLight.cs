7dcc4b2dc577c7bfb9f5f9efb582dc1dddb40007
qqtry-gg <wojtek.bielak1@interia.pl>
2025-09-23 20:28:18 +0200

    fixing range

--- something is watching/Assets/Scripts/pickUpFlashLight.cs
+++ something is watching/Assets/Scripts/pickUpFlashLight.cs
@@ -20,7 +20,7 @@ public class pickUpFlashLight : MonoBehaviour
         {
             Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
             RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, 25f))
+            if (Physics.Raycast(ray, out hit, 3f))
             {
                 if (hit.collider.gameObject == flashLight)
                 {
