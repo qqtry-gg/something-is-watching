db925daed71d0bf2145951155dcdcc002a1a8848
qqtry-gg <wojtek.bielak1@interia.pl>
2025-09-26 19:42:06 +0200

    added really simple enemy ai

--- something is watching/Assets/MonsterScript.cs
+++ something is watching/Assets/MonsterScript.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MonsterScript : MonoBehaviour
 {
+    [SerializeField] NavMeshAgent monsterAI;
+    [SerializeField] Transform player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,6 @@ public class MonsterScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        monsterAI.SetDestination(player.position);
     }
 }
